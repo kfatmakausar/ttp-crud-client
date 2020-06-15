44571696300f7da09665f134344c5477bae6b9ae
Fatma Kausar <kfatmakausar@gmail.com>
2020-06-15 08:47:13 -0400

    routes view edited

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import {
   AllCampusesContainer,
+  AllStudentContainer,
   CampusContainer,
   AddCampusFormContainer,
+  AddStudentFormConatiner,
   EditCampusFormContainer,
+  EditStudentFromContainer,
+  StudentContainer,
 } from "../containers";
 
 const RoutesView = () => {
@@ -12,13 +16,13 @@ const RoutesView = () => {
     <Switch>
       <Route exact path="/" component={AllCampusesContainer} />
       <Route exact path="/campuses" component={AllCampusesContainer} />
+      <Route exact path="/students" component={AllStudentContainer} />
       <Route exact path="/campuses/new" component={AddCampusFormContainer} />
+      <Route exact path="/students/new" component={AddStudentFormConatiner} />
       <Route exact path="/campuses/:id" component={CampusContainer} />
-      <Route
-        exact
-        path="/campuses/:id/edit"
-        component={EditCampusFormContainer}
-      />
+      <Route exact path="/students/:id" component={StudentContainer} />
+      <Route exact path="/campuses/:id/edit" component={EditCampusFormContainer} />
+      <Route exact path="/students/:id/edit" component={EditStudentFromContainer} />
     </Switch>
   );
 };
